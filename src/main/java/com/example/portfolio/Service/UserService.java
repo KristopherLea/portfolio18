bf191d8b41de0defdd63294836a720bf0b4454e3
Kristopher Lea <kristopherlea187@gmail.com>
2020-11-11 20:43:56 -0600

    adds getUser and postUser methods in service package

--- src/main/java/com/example/portfolio/Service/UserService.java
+++ src/main/java/com/example/portfolio/Service/UserService.java
@@ -3,5 +3,7 @@ package com.example.portfolio.Service;
 import com.example.portfolio.Model.User;
 
 public interface UserService {
+    public User getUser();
+    public void postUser(User user);
     public Iterable<User> getAllUsers();
 }
