0b5693c2860bd52150a9d4c87cb39ebb10b27897
Kristopher Lea <kristopherlea187@gmail.com>
2020-11-11 21:45:00 -0600

    changed methods in service

--- src/main/java/com/example/portfolio/Controller/UserController.java
+++ src/main/java/com/example/portfolio/Controller/UserController.java
@@ -4,6 +4,7 @@ import com.example.portfolio.Model.User;
 import com.example.portfolio.Service.UserServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,5 +18,12 @@ public class UserController {
     @GetMapping("user")
     public Iterable<User> getAllUsers(){
         return userServiceImpl.getAllUsers();
+    }
+
+    @PostMapping("/create/user")
+    public String postUser(){
+        User user = userServiceImpl.getUser();
+        userServiceImpl.postUser(user);
+        return "fetched and created";
     }
 }
